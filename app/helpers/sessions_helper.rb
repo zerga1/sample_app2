00e38f651e16822c58176faa138fee8271c43c8d
zerga1 <zerzagabor@gmail.com>
2013-10-22 23:00:48 +0200

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,12 @@
 module SessionsHelper
 
   def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
-    self.current_user = user
+    cookies.permanent[:remember_token] = user.remember_token
+    current_user = user
   end
 
   def signed_in?
     !current_user.nil?
-  end
-
-  def sign_out
-    self.current_user = nil
-    cookies.delete(:remember_token)
   end
 
   def current_user=(user)
@@ -21,7 +14,24 @@ module SessionsHelper
   end
 
   def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def sign_out
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022171711) do
+ActiveRecord::Schema.define(version: 20131022204853) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20131022171711) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
