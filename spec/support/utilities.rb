33dfe1f8c7b903bf264c7df70942234054bc7f4c
zerga1 <zerzagabor@gmail.com>
2013-11-03 00:26:18 +0100

    bugfix

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -15,9 +15,9 @@ end
 def sign_in(user, options={})
   if options[:no_capybara]
     # Sign in when not using Capybara.
-    remember_token = User.new_remember_token
+    remember_token = User.encrypt(User.new_remember_token)
     cookies[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    user.update_attribute(:remember_token, remember_token)
   else
     visit signin_path
     fill_in "Email",    with: user.email
