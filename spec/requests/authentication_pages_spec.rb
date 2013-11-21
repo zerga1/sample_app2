337cfdfb2397516fa3994adb41c2849cef1ef9e6
zerga1 <zerzagabor@gmail.com>
2013-11-21 15:17:54 +0100

    bugfix

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -135,12 +135,12 @@ describe "Authentication" do
       describe "submitting a GET request to the Users#edit action" do
         before { get edit_user_path(wrong_user) }
         specify { expect(response.body).not_to match(full_title('Edit user')) }
-        specify { expect(response).to redirect_to(signin_url) }
+        specify { expect(response).to redirect_to(root_url) }
       end
 
       describe "submitting a PATCH request to the Users#update action" do
         before { patch user_path(wrong_user) }
-        specify { expect(response).to redirect_to(signin_url) }
+        specify { expect(response).to redirect_to(root_url) }
       end
     end
     describe "as non-admin user" do
@@ -151,7 +151,7 @@ describe "Authentication" do
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
-        specify { expect(response).to redirect_to(signin_url) }
+        specify { expect(response).to redirect_to(root_url) }
       end
     end
   end
